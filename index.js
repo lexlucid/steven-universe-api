f5afd30415abe395952fad2287faef3ff13071bb
Lex Garey-Fernandez <lexgarey@gmail.com>
2023-06-17 17:34:26 -0600

    Update routes

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const cors = require("cors")
+const PORT = process.env.PORT || 3000
 
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Watermelon Steven!')
+app.use(cors())
+app.use(express.static('public'))
+
+let characters = {
+    "gems": [
+        {  
+        "name": "Steven Universe",
+        "species": "Half-human, half-gem",
+        "origin": "Beach City"
+        },
+        {
+            "name": "Pearl",
+            "species": "Gem",
+            "origin": "Homeworld"
+        },
+        {
+            "name": "Garnet",
+            "species": "Gem Fusion",
+            "origin": "Homeworld"
+        },
+        {
+            "name": "Amethyst",
+            "species": "Gem",
+            "origin": "Homeworld"
+        },
+    ],
+    "humans": [
+        {
+            "name": "Connie",
+            "origin": "Beach City"
+        },
+        {
+            "name": "Greg Universe",
+            "origin": "Beach City"
+        }
+    ],
+    "unknown": {
+        "endpoint": "unknown"
+    }
+}
+
+app.get('/', (req, res) => {
+    // response.sendFile(__dirname + '/index.html')
+    res.send("Watermelon Steven!")
+    console.log("I'm working!")
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/characters', (request, response) => {
+    response.json(characters)
+})
+
+// app.get('/api/:gems', (request, response) => {
+//     const gems = request.params.gems.toLowerCase()
+//     console.log(request.params.gems)
+//     response.json(characters.gems)
+// })
+
+// app.get('/api/:humans', (request, response) => {
+//     const humans = request.params.humans.toLowerCase()
+//     console.log(request.params.humans)
+//     response.json(characters.humans)
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+
+
+
+
